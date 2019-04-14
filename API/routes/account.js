d75b429cb26c6fba2412006240d5709b0c6a3397
shemaerc <ricshama@gmail.com>
2019-04-14 09:52:15 +0200

    [feature #165332923] adding account route

---
+++ API/routes/account.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import Account from '../controllers/account';
+const router = express.Router();
+router.get('/accounts', Account.getAccounts);
+router.get('/accounts/:accn', Account.getAccount);
+router.post('/accounts', Account.createAccount);
+router.patch('/accounts/edit/:id', Account.updateAccount);
+router.patch('/accounts/:accNumber',Account.activeDeactive);
+router.delete('/accounts/:accNo',Account.accountDelete)
+// export default router;
+module.exports = router;
